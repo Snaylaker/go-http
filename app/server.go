6a49263ded262c877f40540708c8acff0859be32
Snaylaker <mehdihimmid73@gmail.com>
2024-04-07 15:01:58 +0200

    :not gonna work but curious

--- app/server.go
+++ app/server.go
@@ -13,24 +13,32 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	con,err := l.Accept()
+	con, err := l.Accept()
 	if err != nil {
-	 	fmt.Println("Error accepting connection: ", err.Error())
+		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	
-	req := make([]byte,1024)
+	req := make([]byte, 1024)
 	con.Read(req)
-	
+
 	parsedResponse := string(req)
 	fmt.Printf(parsedResponse)
-	
-	if s.Contains(parsedResponse,"/ "){
+
+	if s.Contains(parsedResponse, "GET /echo/") {
+		param := s.Split(parsedResponse, " ")
+		url := s.Split(param[1], "/")
+		resp := []byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n" + url[len(url)-1])
+		con.Write(resp)
+		os.Exit(1)
+	}
+
+	if s.Contains(parsedResponse, "/ ") {
 		resp := []byte("HTTP/1.1 200 OK\r\n\r\n")
 		con.Write(resp)
+		os.Exit(1)
 	}
 	resp := []byte("HTTP/1.1 404 Not Found\r\n\r\n")
 	con.Write(resp)
+	os.Exit(1)
 }
