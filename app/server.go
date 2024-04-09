c2a28129c2276cee6366dafe7a4bb7e22be5c973
Snaylaker <mehdihimmid73@gmail.com>
2024-04-09 17:46:18 +0200

    rawdog

--- app/server.go
+++ app/server.go
@@ -51,6 +51,19 @@ func handleConnection(con net.Conn, path string) {
 			response = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + strconv.Itoa(len(fi)) + "\r\n\r\n"
 			con.Write(append([]byte(response), fi...))
 		}
+	} else if strings.HasPrefix(parsedResponse, "POST /files/") {
+		param := strings.Split(parsedResponse, " ")
+		fmt.Print(param)
+		url := strings.TrimPrefix(param[1], "/files/")
+		filePath := path + `/` + url
+		fi, err := os.ReadFile(filePath)
+		if err != nil {
+			response = "HTTP/1.1 404 Not Found\r\n\r\n"
+			con.Write([]byte(response))
+		} else {
+			response = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + strconv.Itoa(len(fi)) + "\r\n\r\n"
+			con.Write(append([]byte(response), fi...))
+		}
 	} else {
 		response = "HTTP/1.1 404 Not Found\r\n\r\n"
 		con.Write([]byte(response))
