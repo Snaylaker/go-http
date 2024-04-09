b5e2412eb128e6adaae63764af6ca3d75aa53e3b
Snaylaker <mehdihimmid73@gmail.com>
2024-04-09 17:15:30 +0200

    rawdog

--- app/server.go
+++ app/server.go
@@ -61,9 +61,10 @@ func handleConnection(con net.Conn) {
 }
 
 func main() {
-	path := *flag.String("directory", "", "path to file")
+	path := *flag.String("directory", "tfk", "path to file")
 	flag.Parse()
-	fmt.Print(" path ", path)
+	fmt.Println(" path :", path)
+
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221:", err)
